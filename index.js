fd88665d34a980b2c923525f94685a396ae53421
SUNDUS-THE-CODER <119600559+SUNDUS-THE-CODER@users.noreply.github.com>
2023-01-17 19:12:15 +0000

    add all required functions

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => [array[0], array[1]];
+const returnLastTwoDrivers = (array) => [array[array.length - 2], array[array.length - 1]];
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (times) => (fare) => fare * times; 
+const fareDoubler = (fare) => fare * 2;
+const fareTripler = (fare) => fare * 3;
+const selectDifferentDrivers = (array, callback) => callback(array);
